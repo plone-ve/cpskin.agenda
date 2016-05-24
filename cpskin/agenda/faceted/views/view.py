3f20164bba0da77b4052be531162df42be37b030
Laurent Lasudry <lasudry@gmail.com>
2016-05-24 14:20:51 +0200

    Add custom events view for faceted
    Events are grouped by dates and by single / multi days

---
+++ cpskin/agenda/faceted/views/view.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+from Products.CMFCore.utils import getToolByName
+from Products.Five.browser import BrowserView
+from zope.component import getMultiAdapter
+
+
+def is_in_range(date, start, end):
+    if start and date < start:
+        return False
+    if end and date > end:
+        return False
+    return True
+
+
+def sort_and_group(context, brains, start, end):
+    """
+    We need to group by day and separate one-day and multi-days events
+    """
+    catalog = getToolByName(context, 'portal_catalog')
+    days = {}
+    for brain in brains:
+        rid = brain.getRID()
+        idx = catalog.getIndexDataForRID(rid)
+        allDates = idx['getDates']
+        multiDays = len(allDates) > 1 and 'multi' or 'single'
+        for date in allDates:
+            if not is_in_range(date, start, end):
+                continue
+            if date in days:
+                days[date][multiDays].append(brain)
+            else:
+                days[date] = {'single': [],
+                              'multi': []}
+                days[date][multiDays].append(brain)
+    return days
+
+
+class EventsView(BrowserView):
+    """
+    """
+
+    def organize(self, results):
+        if not results:
+            return results
+        startDate = None
+        endDate = None
+        handler = getMultiAdapter((self.context, self.request),
+                                  name=u'faceted_query')
+        criteria = handler.criteria()
+        if 'start' in criteria and 'end' in criteria:
+            endDate = criteria['start']['query'].asdatetime().date()
+            startDate = criteria['end']['query'].asdatetime().date()
+        results = sort_and_group(self.context, results, startDate, endDate)
+        return results
