7246fb9786467bb645d61d86766d3927b7824bbf
Benoît Suttor <bsuttor@imio.be>
2016-05-31 15:12:42 +0200

    apply remove event attendees when adding cpkin agenda behavior

--- cpskin/agenda/behaviors/related_contacts.py
+++ cpskin/agenda/behaviors/related_contacts.py
@@ -1,20 +1,17 @@
 # -*- coding: utf-8 -*-
 from collective.contact.widget.schema import ContactList, ContactChoice
 from collective.contact.widget.source import ContactSourceBinder
+from cpskin.core.utils import add_behavior
+from cpskin.core.utils import remove_behavior
 from cpskin.locales import CPSkinMessageFactory as _
 from plone.autoform.interfaces import IFormFieldProvider
-from plone.supermodel import directives
+# from plone.supermodel import directives
 from plone.supermodel import model
 from zope.interface import provider
 
 
 @provider(IFormFieldProvider)
 class IRelatedContacts(model.Schema):
-    directives.fieldset(
-        'categorization',
-        label=_(u'label_schema_categorization', default=u'Categorization'),
-        fields=('location', 'organizer', 'contact', 'partner'),
-    )
 
     location = ContactChoice(
         title=_(u"Location"),
@@ -50,3 +47,14 @@ class IRelatedContacts(model.Schema):
         ),
         required=False,
     )
+
+
+def modified_event(obj, event):
+    type_name = obj.id
+    if type_name == "Event":
+        if 'cpskin.agenda.behaviors.related_contacts.IRelatedContacts' in obj.behaviors:
+            remove_behavior(
+                type_name, 'plone.app.event.dx.behaviors.IEventAttendees')
+        else:
+            add_behavior(
+                type_name, 'plone.app.event.dx.behaviors.IEventAttendees')
