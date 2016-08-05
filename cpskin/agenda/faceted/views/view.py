a157b6bf56df55c96b391cc84bb9dd327597440d
Laurent Lasudry <lasudry@gmail.com>
2016-08-05 16:11:38 +0200

    Override eea.facetednavigation query code to handle events pagination

--- cpskin/agenda/faceted/views/view.py
+++ cpskin/agenda/faceted/views/view.py
@@ -1,41 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from Products.Five.browser import BrowserView
-from datetime import timedelta
-from eea.facetednavigation.interfaces import ICriteria
-from plone import api
 from zope.component import getMultiAdapter
-
-
-def is_in_range(date, start, end):
-    if start and date < start:
-        return False
-    if end and date > end:
-        return False
-    return True
-
-
-def sort_and_group(context, brains, start, end):
-    """
-    We need to group by day and separate one-day and multi-days events
-    """
-    catalog = api.portal.get_tool('portal_catalog')
-    days = {}
-    for brain in brains:
-        rid = brain.getRID()
-        idx = catalog.getIndexDataForRID(rid)
-        allDates = idx['event_dates']
-        multiDays = len(allDates) > 1 and 'multi' or 'single'
-        for date in allDates:
-            if not is_in_range(date, start, end):
-                continue
-            if date in days:
-                days[date][multiDays].append(brain)
-            else:
-                days[date] = {'single': [],
-                              'multi': []}
-                days[date][multiDays].append(brain)
-    return days
 
 
 class EventsView(BrowserView):
@@ -50,31 +16,3 @@ class EventsView(BrowserView):
         view = getMultiAdapter((obj, request),
                                 name=u'faceted-event-preview-item')
         return view and view() or ''
-
-    def organize(self, results):
-        if not results:
-            return {}
-        startDate = None
-        endDate = None
-        handler = getMultiAdapter((self.context, self.request),
-                                  name=u'faceted_query')
-        criteria = handler.criteria()
-        if 'start' in criteria and 'end' in criteria:
-            endDate = criteria['start']['query'].asdatetime().date()
-            startDate = criteria['end']['query'].asdatetime().date()
-            # Faceted use previous day at 23:59:59 for its query
-            startDate = startDate + timedelta(days=1)
-        results = sort_and_group(self.context, results, startDate, endDate)
-        return results
-
-    def perPage(self):
-        num_per_page = 20
-        criteria = ICriteria(self.context)
-        for cid, criterion in criteria.items():
-            widgetclass = criteria.widget(cid=cid)
-            widget = widgetclass(self.context, self.request, criterion)
-            if widget.widget_type == 'resultsperpage':
-                kwargs = dict((key.replace('[]', ''), val)
-                              for key, val in self.request.form.items())
-                num_per_page = widget.results_per_page(kwargs)
-        return num_per_page
