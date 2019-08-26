8f6cef44aa4860f0d39b6e269e2f864a5376fb8f
Christophe Boulanger <christophe.boulanger@imio.be>
2019-08-26 10:16:16 +0200

    Remove some extra js

--- cpskin/agenda/browser/resources/eventbooking.js
+++ cpskin/agenda/browser/resources/eventbooking.js
@@ -1,19 +1,6 @@
 // jquery for search button in navigation 
 
 $( document ).ready(function() {
-       $( ".btn-search" ).click(function(e) {
-        $( "#hidden-search" ).slideToggle(
-        "fast",
-        function () {
-               $("#searchGadget").focus();
-           });
-         e.preventDefault();
-        });
-       
-       $("#portal-globalnav a[tabindex]").click(function(){
-        $("#hidden-search").hide('fast');
-       });
-
        $(".toggle-button").click(function(){
          var toggle_button_id = this.id;
          var toggle_block_id = toggle_button_id.replace("-button", "-block");
