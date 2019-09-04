2b17190e093d2781ed8ae4f1ff7bea475ea935e3
Christophe Boulanger <christophe.boulanger@imio.be>
2019-09-04 11:00:36 +0200

    Load image_banner img (with good scale) if exist else load leadimage (with good scale) if exist else no img!

--- cpskin/agenda/browser/event_booking.py
+++ cpskin/agenda/browser/event_booking.py
@@ -1,7 +1,32 @@
 from cpskin.agenda.browser.event_summary import EventContactSummaryView
-
+from zope.annotation.interfaces import IAnnotations
+from plone.app.imagecropping import PAI_STORAGE_KEY
 
 class EventBooking(EventContactSummaryView):
     """
     """
 
+    def get_good_banner(self):
+        img = None
+        if self.context.image_banner and self.has_crop(self.context, 'image_banner', 'banner_event'):
+            img = '@@images/image_banner/banner_event'
+        if img is None and self.context.image_banner and self.has_crop(self.context, 'image_banner', 'banner'):
+            img = '@@images/image_banner/banner'
+        if img is None and self.context.image_banner:
+            img = '@@images/image_banner'
+        if img is None and self.context.image and self.has_crop(self.context, 'image', 'banner_event'):
+            img = '@@images/image/banner_event'
+        if img is None and self.context.image and self.has_crop(self.context, 'image', 'banner'):
+            img = '@@images/image/banner'
+        if img is None and self.context.image:
+            img = '@@images/image'
+        if img is None:
+            return img
+        else:
+            return '{}/{}'.format(self.data.url, img)
+
+    def has_crop(self, obj, fieldname, scale):
+        crops = IAnnotations(obj).get(PAI_STORAGE_KEY)
+        if not crops:
+            return False
+        return '{0:s}_{1:s}'.format(fieldname, scale) in crops
