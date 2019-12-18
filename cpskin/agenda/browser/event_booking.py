d43e256d0daeefc1e3857659d08193079c2cadd0
Christophe Boulanger <christophe.boulanger@imio.be>
2019-12-18 11:37:48 +0100

    Prevent an error when encoding "None" location. (+ black file)

--- cpskin/agenda/browser/event_booking.py
+++ cpskin/agenda/browser/event_booking.py
@@ -9,29 +9,47 @@ class EventBooking(EventContactSummaryView):
 
     def get_good_banner(self):
         img = None
-        if self.context.image_header and self.has_crop(self.context, 'image_header', 'banner_event'):
-            img = '@@images/image_header/banner_event'
-        if img is None and self.context.image_header and self.has_crop(self.context, 'image_header', 'banner'):
-            img = '@@images/image_header/banner'
+        if self.context.image_header and self.has_crop(
+            self.context, "image_header", "banner_event"
+        ):
+            img = "@@images/image_header/banner_event"
+        if (
+            img is None
+            and self.context.image_header
+            and self.has_crop(self.context, "image_header", "banner")
+        ):
+            img = "@@images/image_header/banner"
         if img is None and self.context.image_header:
-            img = '@@images/image_header'
-        if img is None and self.context.image and self.has_crop(self.context, 'image', 'banner_event'):
-            img = '@@images/image/banner_event'
-        if img is None and self.context.image and self.has_crop(self.context, 'image', 'banner'):
-            img = '@@images/image/banner'
+            img = "@@images/image_header"
+        if (
+            img is None
+            and self.context.image
+            and self.has_crop(self.context, "image", "banner_event")
+        ):
+            img = "@@images/image/banner_event"
+        if (
+            img is None
+            and self.context.image
+            and self.has_crop(self.context, "image", "banner")
+        ):
+            img = "@@images/image/banner"
         if img is None and self.context.image:
-            img = '@@images/image'
+            img = "@@images/image"
         if img is None:
             return img
         else:
-            return '{}/{}'.format(self.data.url, img)
+            return "{}/{}".format(self.data.url, img)
 
     def has_crop(self, obj, fieldname, scale):
         crops = IAnnotations(obj).get(PAI_STORAGE_KEY)
         if not crops:
             return False
-        return '{0:s}_{1:s}'.format(fieldname, scale) in crops
+        return "{0:s}_{1:s}".format(fieldname, scale) in crops
 
     def get_str_location(self):
         location = super(EventBooking, self).get_location()
-        return '{}: {} {}, {} {}'.format(location.Title(), location.number, location.street.encode('utf-8'), location.zip_code, location.city.encode('utf-8'))
+        city = location.city.encode("utf-8") if location.city else u""
+        street = location.street.encode("utf-8") if location.street else u""
+        return "{}: {} {}, {} {}".format(
+            location.Title(), location.number, street, location.zip_code, city
+        )
